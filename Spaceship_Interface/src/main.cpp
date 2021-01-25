7722bb14313f4f08d46a417d05bdbc7a4b8d5cfb
itsgosho <itsgosho2@gmail.com>
2021-01-25 20:28:55 +0200

    The PIN numbers are no longer magic numbers

--- Spaceship_Interface/src/main.cpp
+++ Spaceship_Interface/src/main.cpp
@@ -7,31 +7,41 @@ int switchState = 0;
 const int SERIAL_BAUD = 9600;
 const int DELAY_INTERVALS_MS = 250;
 
-void setup() {
+const int LED_1_PIN_NUMBER = 3;
+const int LED_2_PIN_NUMBER = 4;
+const int LED_3_PIN_NUMBER = 5;
+const int SWITCH_STATE_PIN_NUMBER = 2;
+
+void setup()
+{
   Serial.begin(SERIAL_BAUD);
-  
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(2, INPUT);
+
+  pinMode(LED_1_PIN_NUMBER, OUTPUT);
+  pinMode(LED_2_PIN_NUMBER, OUTPUT);
+  pinMode(LED_3_PIN_NUMBER, OUTPUT);
+  pinMode(SWITCH_STATE_PIN_NUMBER, INPUT);
 }
 
-void loop() {
-  switchState = digitalRead(2);
+void loop()
+{
+  switchState = digitalRead(SWITCH_STATE_PIN_NUMBER);
 
-  if(switchState == LOW) {
-    digitalWrite(3, HIGH);
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
-  }else {
-    digitalWrite(3, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(5, HIGH);
+  if (switchState == LOW)
+  {
+    digitalWrite(LED_1_PIN_NUMBER, HIGH);
+    digitalWrite(LED_2_PIN_NUMBER, LOW);
+    digitalWrite(LED_3_PIN_NUMBER, LOW);
+  }
+  else
+  {
+    digitalWrite(LED_1_PIN_NUMBER, LOW);
+    digitalWrite(LED_2_PIN_NUMBER, LOW);
+    digitalWrite(LED_3_PIN_NUMBER, HIGH);
 
     delay(DELAY_INTERVALS_MS);
 
-    digitalWrite(4, HIGH);
-    digitalWrite(5, LOW);
+    digitalWrite(LED_2_PIN_NUMBER, HIGH);
+    digitalWrite(LED_3_PIN_NUMBER, LOW);
 
     delay(DELAY_INTERVALS_MS);
   }
