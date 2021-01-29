d081092343d9131943baff2cc2f83e6875232000
itsgosho <itsgosho2@gmail.com>
2021-01-29 14:19:28 +0200

    Solution for the Love-o-Meter problem

--- Love-o-Meter/src/main.cpp
+++ Love-o-Meter/src/main.cpp
@@ -1,9 +1,50 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+const int sensorPin = A0;
+const float baselineTemp = 20.0;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  for (int pinNumber = 2; pinNumber < 5; pinNumber++)
+  {
+    pinMode(pinNumber, OUTPUT);
+    digitalWrite(pinNumber, LOW);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  int sensorVal = analogRead(sensorPin);
+  Serial.print("Sensor Value: ");
+  Serial.print(sensorVal);
+
+  float voltage = (sensorVal / 1024.0) * 5.0;
+  Serial.print(", Volts: ");
+  Serial.print(voltage);
+  Serial.print(", degrees C: ");
+  float temperature = (voltage - .5) * 100;
+  Serial.println(temperature);
+
+  if (temperature < baselineTemp + 2)
+  {
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+  }
+  else if (temperature >= baselineTemp + 2 && temperature < baselineTemp + 4)
+  {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+  }
+  else if (temperature >= baselineTemp + 6)
+  {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+  }
+
+  delay(1);
 }
