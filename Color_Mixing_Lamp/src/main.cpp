2d51e347b4cb483dd02da0593653702788b0c3cc
itsgosho <itsgosho2@gmail.com>
2021-01-31 00:32:35 +0200

    Changed the constant variables to uppercase and their names to be more descriptive. The logs will add \n at the end for a new line.

--- Color_Mixing_Lamp/src/main.cpp
+++ Color_Mixing_Lamp/src/main.cpp
@@ -2,13 +2,13 @@
 #include <SerialPrintF.h>
 
 //For pin numbers we don't need anything more than 256 :D
-const short redLEDPin = 10;
-const short greenLEDPin = 9;
-const short blueLEDPin = 11;
+const short RGB_LED_RED_PIN_NUMBER = 9;
+const short RGB_LED_GREEN_PIN_NUMBER = 10;
+const short RGB_LED_BLUE_PIN_NUMBER = 11;
 
-const short redSensorPin = A0;
-const short greenSensorPin = A1;
-const short blueSensorPin = A2;
+const short PHOTORESISTOR_RED_PIN_NUMBER = A0;
+const short PHOTORESISTOR_GREEN_PIN_NUMBER = A1;
+const short PHOTORESISTOR_BLUE_PIN_NUMBER = A2;
 
 int redValue = 0;
 int greenValue = 0;
@@ -22,28 +22,29 @@ void setup()
 {
   Serial.begin(9600);
 
-  pinMode(greenLEDPin, OUTPUT);
-  pinMode(redLEDPin, OUTPUT);
-  pinMode(blueLEDPin, OUTPUT);
+  pinMode(RGB_LED_RED_PIN_NUMBER, OUTPUT);
+  pinMode(RGB_LED_GREEN_PIN_NUMBER, OUTPUT);
+  pinMode(RGB_LED_BLUE_PIN_NUMBER, OUTPUT);
 }
 
 void loop()
 {
-  redSensorValue = analogRead(redSensorPin);
+  redSensorValue = analogRead(PHOTORESISTOR_RED_PIN_NUMBER);
   delay(5);
-  greenSensorValue = analogRead(greenSensorPin);
+  greenSensorValue = analogRead(PHOTORESISTOR_GREEN_PIN_NUMBER);
   delay(5);
-  blueSensorValue = analogRead(blueSensorPin);
+  blueSensorValue = analogRead(PHOTORESISTOR_BLUE_PIN_NUMBER);
 
-  serial_printf(Serial, "Raw Sensor Values  Red: %d Green: %d Blue: %d", redSensorValue, greenSensorValue, blueSensorValue);
+  serial_printf(Serial, "Raw Sensor Values  Red: %d Green: %d Blue: %d\n", redSensorValue, greenSensorValue, blueSensorValue);
 
   redValue = redSensorValue / 4;
   greenValue = greenSensorValue / 4;
   blueValue = blueSensorValue / 4;
 
-  serial_printf(Serial, "Mapped Sensor Values  Red: %d Green: %d Blue: %d", redValue, greenValue, blueValue);
+  serial_printf(Serial, "Mapped Sensor Values  Red: %d Green: %d Blue: %d\n", redValue, greenValue, blueValue);
 
-  analogWrite(redLEDPin, redValue);
-  analogWrite(greenLEDPin, greenValue);
-  analogWrite(blueLEDPin, blueValue);
+  analogWrite(RGB_LED_RED_PIN_NUMBER, redValue);
+  analogWrite(RGB_LED_GREEN_PIN_NUMBER, greenValue);
+  analogWrite(RGB_LED_BLUE_PIN_NUMBER, blueValue);
+  delay(1000);
 }
