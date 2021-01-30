3276747974d55ef00d4b12f12ba579c4a3539650
itsgosho <itsgosho2@gmail.com>
2021-01-30 23:58:19 +0200

    Uploaded the solution for the Color Mixing Lamp

--- Color_Mixing_Lamp/src/main.cpp
+++ Color_Mixing_Lamp/src/main.cpp
@@ -1,9 +1,49 @@
 #include <Arduino.h>
+#include <SerialPrintF.h>
 
-void setup() {
-  // put your setup code here, to run once:
+//For pin numbers we don't need anything more than 256 :D
+const int redLEDPin = 10;
+const int greenLEDPin = 9;
+const int blueLEDPin = 11;
+
+const int redSensorPin = A0;
+const int greenSensorPin = A1;
+const int blueSensorPin = A2;
+
+int redValue = 0;
+int greenValue = 0;
+int blueValue = 0;
+
+int redSensorValue = 0;
+int greenSensorValue = 0;
+int blueSensorValue = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  pinMode(greenLEDPin, OUTPUT);
+  pinMode(redLEDPin, OUTPUT);
+  pinMode(blueLEDPin, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  redSensorValue = analogRead(redSensorPin);
+  delay(5);
+  greenSensorValue = analogRead(greenSensorPin);
+  delay(5);
+  blueSensorValue = analogRead(blueSensorPin);
+
+  serial_printf(Serial, "Raw Sensor Values  Red: %d Green: %d Blue: %d", redSensorValue, greenSensorValue, blueSensorValue);
+
+  redValue = redSensorValue / 4;
+  greenValue = greenSensorValue / 4;
+  blueValue = blueSensorValue / 4;
+
+  serial_printf(Serial, "Mapped Sensor Values  Red: %d Green: %d Blue: %d", redValue, greenValue, blueValue);
+
+  analogWrite(redLEDPin, redValue);
+  analogWrite(greenLEDPin, greenValue);
+  analogWrite(blueLEDPin, blueValue);
 }
