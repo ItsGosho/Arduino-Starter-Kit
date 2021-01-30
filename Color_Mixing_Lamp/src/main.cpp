93ccea9c93378869a717c807071d450427488708
itsgosho <itsgosho2@gmail.com>
2021-01-31 01:36:34 +0200

    Exposed method for setting the component's value (RGB)

--- Color_Mixing_Lamp/src/main.cpp
+++ Color_Mixing_Lamp/src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include <SerialPrintF.h>
 
-//For pin numbers we don't need anything more than 256 :D
 const short RGB_LED_RED_PIN_NUMBER = 9;
 const short RGB_LED_GREEN_PIN_NUMBER = 10;
 const short RGB_LED_BLUE_PIN_NUMBER = 11;
@@ -9,6 +8,8 @@ const short RGB_LED_BLUE_PIN_NUMBER = 11;
 const short PHOTORESISTOR_RED_PIN_NUMBER = A0;
 const short PHOTORESISTOR_GREEN_PIN_NUMBER = A1;
 const short PHOTORESISTOR_BLUE_PIN_NUMBER = A2;
+
+void setRGBLEDValue(int, int, int);
 
 void setup()
 {
@@ -35,8 +36,13 @@ void loop()
 
   serial_printf(Serial, "Mapped Sensor Values  Red: %d Green: %d Blue: %d\n", redValue, greenValue, blueValue);
 
-  analogWrite(RGB_LED_RED_PIN_NUMBER, redValue);
-  analogWrite(RGB_LED_GREEN_PIN_NUMBER, greenValue);
-  analogWrite(RGB_LED_BLUE_PIN_NUMBER, blueValue);
+  setRGBLEDValue(redValue, greenValue, blueValue);
   delay(1000);
+}
+
+void setRGBLEDValue(int r, int g, int b)
+{
+  analogWrite(RGB_LED_RED_PIN_NUMBER, r);
+  analogWrite(RGB_LED_GREEN_PIN_NUMBER, g);
+  analogWrite(RGB_LED_BLUE_PIN_NUMBER, b);
 }
