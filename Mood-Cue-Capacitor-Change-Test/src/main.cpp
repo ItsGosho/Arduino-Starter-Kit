20baa41eea37da4ec91fa5d11e8f9a90c3863211
itsgosho <itsgosho2@gmail.com>
2021-02-04 13:28:53 +0200

    Exposed the logic for the seconds check into separate method

--- Mood-Cue-Capacitor-Change-Test/src/main.cpp
+++ Mood-Cue-Capacitor-Change-Test/src/main.cpp
@@ -8,52 +8,64 @@ const short POTENTIOMETER_PIN_NUMBER = A0;
 const short SERVO_MOTOR_PIN_NUMBER = 9;
 const int SERIAL_BAUD = 9600;
 
-int lastSecond = 0;
-
 int minPotValueLastSec = 1023;
 int maxPotValueLastSec = 0;
 int minServoAngleLastSec = 179;
 int maxServoAngleLastSec = 0;
 
+bool hasSecondPassed();
+
 void setup()
 {
-    Serial.begin(SERIAL_BAUD);
+  Serial.begin(SERIAL_BAUD);
 
-    servoMotor.attach(SERVO_MOTOR_PIN_NUMBER);
+  servoMotor.attach(SERVO_MOTOR_PIN_NUMBER);
 }
 
 void loop()
 {
-    int potentiometerValue = analogRead(POTENTIOMETER_PIN_NUMBER);
-    int servoAngle = map(potentiometerValue, 0, 1023, 0, 179);
+  int potentiometerValue = analogRead(POTENTIOMETER_PIN_NUMBER);
+  int servoAngle = map(potentiometerValue, 0, 1023, 0, 179);
 
-    if (potentiometerValue < minPotValueLastSec)
-        minPotValueLastSec = potentiometerValue;
+  if (potentiometerValue < minPotValueLastSec)
+    minPotValueLastSec = potentiometerValue;
 
-    if (potentiometerValue > maxPotValueLastSec)
-        maxPotValueLastSec = potentiometerValue;
+  if (potentiometerValue > maxPotValueLastSec)
+    maxPotValueLastSec = potentiometerValue;
 
-    if (servoAngle < minServoAngleLastSec)
-        minServoAngleLastSec = servoAngle;
+  if (servoAngle < minServoAngleLastSec)
+    minServoAngleLastSec = servoAngle;
 
-    if (servoAngle > maxServoAngleLastSec)
-        maxServoAngleLastSec = servoAngle;
+  if (servoAngle > maxServoAngleLastSec)
+    maxServoAngleLastSec = servoAngle;
 
-    long currentMillis = millis();
-    int second = currentMillis / 1000;
+  if (hasSecondPassed())
+  {
+    serial_printf(Serial, "\nPotentiometer MIN: %d MAX: %d, Servo MIN: %d MAX: %d", minPotValueLastSec, maxPotValueLastSec, minServoAngleLastSec, maxServoAngleLastSec);
+    minPotValueLastSec = 1023;
+    maxPotValueLastSec = 0;
+    minServoAngleLastSec = 179;
+    maxServoAngleLastSec = 0;
+  }
 
-    if (second > lastSecond)
-    {
-        lastSecond = second;
-        serial_printf(Serial, "\nPotentiometer MIN: %d MAX: %d, Servo MIN: %d MAX: %d", minPotValueLastSec, maxPotValueLastSec, minServoAngleLastSec, maxServoAngleLastSec);
-        minPotValueLastSec = 1023;
-        maxPotValueLastSec = 0;
-        minServoAngleLastSec = 179;
-        maxServoAngleLastSec = 0;
-    }
+  //serial_printf(Serial, "\nPotentiometer Value: %d, Servo Angle: %d", potentiometerValue, servoAngle);
 
-    //serial_printf(Serial, "\nPotentiometer Value: %d, Servo Angle: %d", potentiometerValue, servoAngle);
+  servoMotor.write(servoAngle);
+  //delay(15);
+}
 
-    servoMotor.write(servoAngle);
-    //delay(15);
+int lastSecond = 0;
+
+bool hasSecondPassed()
+{
+  long currentMillis = millis();
+  int second = currentMillis / 1000;
+
+  if (second > lastSecond)
+  {
+    lastSecond = second;
+    return true;
+  }
+
+  return false;
 }
