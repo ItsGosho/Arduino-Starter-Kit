a6edf434f88ace887f0b57b97e73f0a71a66f0b0
itsgosho <itsgosho2@gmail.com>
2021-05-24 23:13:06 +0300

    Added the solution for the motorized pinwheel

---
+++ Motorized-Pinwheel/src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+
+const int switchPin = 13;
+const int motorPin = 9;
+int switchState = 0;
+
+void setup() {
+  pinMode(motorPin, OUTPUT);
+  pinMode(switchPin, INPUT);
+}
+
+void loop() {
+  switchState = digitalRead(switchPin);
+
+  if(switchState == HIGH) {
+    digitalWrite(motorPin, HIGH);
+  } else {
+    digitalWrite(motorPin, LOW);
+  }
+}
